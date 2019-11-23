910455b31dd498e51809d2ed662b5cc1f83e31e5
Julien <julien.moulis@moulis.me>
2019-11-23 15:21:47 +0100

    PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  apps: [
+    {
+      name: 'API',
+      script: 'back/index.js',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'budget',
+      script: 'npm',
+      args: 'run start:production',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '93.90.204.229',
+      ref: 'origin/master',
+      repo: 'git@github.com:jMoulis/budget.git',
+      path: '/var/www/html/budget/production',
+      ssh_options: ['ForwardAgent=yes'],
+      'post-deploy':
+        'npm install && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
